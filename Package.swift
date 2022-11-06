2ceacad76a84bd1181ae0f7a631d079cae84b5a0
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2022-11-06 12:24:39 +0530

    style: add swift-format for code formatting

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "ValidatableKit",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2),
+        .macCatalyst(.v13)
+    ],
+    products: [
+        .library(name: "ValidatableKit", targets: ["ValidatableKit"]),
+    ],
+    targets: [
+        .target(name: "ValidatableKit", dependencies: []),
+        .testTarget(name: "ValidatableKitTests", dependencies: ["ValidatableKit"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
